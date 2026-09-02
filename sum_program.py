5a77fa8c8a8aef35919b6cc4eb90a08ad94d3dab
mahesh-007p <maheshgaming599@gmail.com>
2026-09-02 16:31:28 +0530

    Add Python File and Jenkins file

---
+++ sum_program.py
@@ -0,0 +1,11 @@
+num1 = 15
+num2 = 25
+
+result = num1 + num2
+
+print("--- Python Sum Application Execution ---")
+print("First Number:", num1)
+print("Second Number:", num2)
+print("The sum is:", result)
+
+
